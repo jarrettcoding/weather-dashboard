925ebb29c0b0db7e5c5ab3a34b57cafbabe243ad
veritascoding <jarrettcoding@gmail.com>
2021-12-19 19:57:09 -0600

    add api key and api url

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + weahterLat + "&lon=" + weatherLon + "&appid=" + weatherApiKey;
+
+var cityApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchedCity + "&limit=1&appid=" + weatherApiKey
+
+var weatherApiKey = "5645be3e6ec32d07c961f83f15ac6bbb";
